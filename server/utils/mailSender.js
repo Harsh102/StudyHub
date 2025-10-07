7566f35be677b3e4c6b734068815023c6642d242
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2025-10-07 10:28:42 +0530

    update code

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,23 +1,31 @@
-const { Resend } = require("resend");
-
-// Initialize Resend with API key from environment variable
-const resend = new Resend("re_Tg3MrZA7_F82oqkKiL5yWSQude5rkQAiz");
+const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
-  try {
-    const data = await resend.emails.send({
-      from: "StudyNotion <sourabhbadgaiya2@gmail.com>", // You can customize sender name & email
-      to: email,
-      subject: title,
-      html: body,
-    });
+    try{
+            let transporter = nodemailer.createTransport({
+                host:process.env.MAIL_HOST,
+                    port: 587,            // or 465 for SSL
+                secure: false,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                }
+            })
 
-    console.log("Email sent:", data.id);
-    return data;
-  } catch (error) {
-    console.error("Mail send error:", error);
-    return null;
-  }
-};
+
+            let info = await transporter.sendMail({
+             from: `"StudyNotion" <${process.env.MAIL_USER}>`,
+                to:`${email}`,
+                subject: `${title}`,
+                html: `${body}`,
+            })
+            // console.log(info);
+            return info;
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+
 
 module.exports = mailSender;
